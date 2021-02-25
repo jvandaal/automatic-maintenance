e7882a1578e00c3da0e6b12e3858c3e22460f219
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2021-02-25 18:24:01 +0100

    chore: add build script

---
+++ build.fsx
@@ -0,0 +1,49 @@
+#r "paket:
+version 6.0.0-beta8
+framework: netstandard20
+source https://api.nuget.org/v3/index.json
+nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 5.0.2 //"
+
+#load "packages/Be.Vlaanderen.Basisregisters.Build.Pipeline/Content/build-generic.fsx"
+
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open ``Build-generic``
+
+let product = "Basisregisters Vlaanderen"
+let copyright = "Copyright (c) Vlaamse overheid"
+let company = "Vlaamse overheid"
+
+let dockerRepository = "registries-maintenance"
+let assemblyVersionNumber = (sprintf "2.%s")
+
+Target.create "Publish" ignore
+Target.create "Containerize" ignore
+Target.create "Push" ignore
+
+Target.create "Publish_RebuildIndexes" (fun _ ->
+  let filterAllFiles = (fun _ -> true)
+  let dockerBuildDirectory = buildDir @@ "RebuildSqlIndexes" @@ "linux"
+  Shell.copyDir dockerBuildDirectory ("src" @@ "sql-server" @@  "rebuild-indexes") filterAllFiles
+)
+
+Target.create "Containerize_RebuildSqlIndexes" (fun _ -> containerize dockerRepository "RebuildSqlIndexes" "rebuild-sql-indexes")
+Target.create "PushContainer_RebuildSqlIndexes" (fun _ -> push dockerRepository "rebuild-sql-indexes")
+
+"NpmInstall"
+==> "Clean"
+==> "Publish_RebuildIndexes"
+==> "Publish"
+
+"Publish"
+==> "Containerize_RebuildSqlIndexes"
+==> "Containerize"
+
+"Containerize"
+==> "DockerLogin"
+==> "PushContainer_RebuildSqlIndexes"
+==> "Push"
+
+Target.runOrDefault "Containerize"

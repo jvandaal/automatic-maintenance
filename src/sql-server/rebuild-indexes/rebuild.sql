f960b0f986dc18f522144135abd5015f82599fcb
David Cumps <david@cumps.be>
2021-03-02 11:27:25 +0100

    style: uppercase sql

--- src/sql-server/rebuild-indexes/rebuild.sql
+++ src/sql-server/rebuild-indexes/rebuild.sql
@@ -1,23 +1,22 @@
 DECLARE @Sql NVARCHAR(MAX);
 
 DECLARE Cur CURSOR LOCAL FAST_FORWARD FOR
-    SELECT 'ALTER INDEX ' + I.name + ' ON ' + S.name + '.' + T.name + ' REBUILD' as 'Statement'
+    SELECT 'ALTER INDEX ' + I.name + ' ON ' + S.name + '.' + T.name + ' REBUILD' AS 'Statement'
     FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL, NULL, NULL) AS DDIPS
-    INNER JOIN sys.tables T on T.object_id = DDIPS.object_id
-    INNER JOIN sys.schemas S on T.schema_id = S.schema_id
+    INNER JOIN sys.tables T ON T.object_id = DDIPS.object_id
+    INNER JOIN sys.schemas S ON T.schema_id = S.schema_id
     INNER JOIN sys.indexes I ON I.object_id = DDIPS.object_id
     AND DDIPS.index_id = I.index_id
     WHERE DDIPS.database_id = DB_ID()
     AND avg_fragmentation_in_percent > 30
-    and I.name is not null
+    AND I.name IS NOT NULL
 
 OPEN Cur
-
   FETCH NEXT FROM Cur INTO @Sql
 
 WHILE (@@FETCH_STATUS = 0)
 BEGIN
-     Exec sp_executesql @Sql
+     EXEC sp_executesql @Sql
      FETCH NEXT FROM Cur INTO @Sql
 END
 
